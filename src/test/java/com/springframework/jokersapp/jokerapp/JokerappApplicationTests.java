bea0ecba52bb39ec0e8e650cb64c3add79b913d6
rsachde <riasachdeva2015@gmail.com>
2020-01-26 22:58:19 +0530

    Initialize the Joker Application.

---
+++ src/test/java/com/springframework/jokersapp/jokerapp/JokerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.jokersapp.jokerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokerappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
